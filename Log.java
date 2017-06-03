f70e7716569e0fc3bbe1f829333d6ddc8634267d
Abhinav Saxena <abhi.saxena96@gmail.com>
2017-06-03 14:34:07 +0530

    Add files via upload

---
+++ Log.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Log
+{
+	 public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the username");
+		sc=nextLine();
+		System.out.println("Enter the Password");
+		sc=nextLine();
+		
+	}
+}
